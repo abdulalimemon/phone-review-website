5ca78f511d490716e2e96353a83ed9dcb0bd8882
Emon02 <abdulalimemon11@gmail.com>
2022-07-09 23:44:48 +0600

    Review Section Complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import "./Home.css";
 import iPhone from '../img/Iphone.jpg';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import useReviewData from '../../hooks/useReviewData';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+    const [reviews, setReviews] = useReviewData();
+
     return (
         <div className="container">
             <div className="row py-5">
@@ -20,7 +25,13 @@ const Home = () => {
             </div>
             <div className="row py-5">
                 <div className="col-md-12">
-                    <h2>Customer Reviews()</h2>
+                    <h2>Customer Reviews(3)</h2>
+                    <div className="row row-cols-1 row-cols-md-3 g-4 py-5">
+                        {
+                            reviews.slice(0, 3).map(review => <ReviewItem key={review.id} review={review}></ReviewItem>)
+                        }
+                    </div>
+                    <button className="btn btn-primary py-2 px-4"><Link className="text-white text-decoration-none" to="/review">See All Review</Link></button>
                 </div>
             </div>
         </div>
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -7,6 +7,7 @@ const Review = () => {
     return (
         <div className="container">
             <div className="row row-cols-1 row-cols-md-3 g-4 py-5">
+                <h2>What Our Customer Say!</h2>
                 {
                     reviews.map(review => <ReviewItem key={review.id} review={review}></ReviewItem>)
                 }
